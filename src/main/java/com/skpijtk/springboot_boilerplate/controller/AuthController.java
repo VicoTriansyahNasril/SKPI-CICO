e5809e3884780e96ba8ad3f0a56c76cd5fb6fd27
VicoTriansyahNasril <vicotriansyahnasril@gmail.com>
2025-06-05 08:02:47 +0700

    Delete Swagger

--- src/main/java/com/skpijtk/springboot_boilerplate/controller/AuthController.java
+++ src/main/java/com/skpijtk/springboot_boilerplate/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.skpijtk.springboot_boilerplate.controller;
 import com.skpijtk.springboot_boilerplate.dto.auth.RegisterRequest;
 import com.skpijtk.springboot_boilerplate.dto.response.ApiResponse;
 import com.skpijtk.springboot_boilerplate.service.AuthService;
-import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +16,6 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/signup")
-    @Operation(summary = "Register Admin", description = "Membuat akun admin baru")
     public ResponseEntity<ApiResponse<?>> registerAdmin(@Valid @RequestBody RegisterRequest request) {
         ApiResponse<?> response = authService.registerAdmin(request);
         return ResponseEntity.ok(response);
